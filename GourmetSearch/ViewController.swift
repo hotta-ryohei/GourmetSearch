81e72e32c9db2d826558b97ba1fe1c6251566cd1
hotta-ryohei <hotta.ryohei@gmail.com>
2024-02-02 10:48:10 +0900

    Initial Commit

---
+++ GourmetSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GourmetSearch
+//
+//  Created by 堀田凌平 on 2024/02/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
