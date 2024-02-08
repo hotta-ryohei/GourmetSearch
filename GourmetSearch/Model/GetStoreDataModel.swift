323b2cbae35ea377821624b3a9cf0a17b20b13fb
hotta-ryohei <hotta.ryohei@gmail.com>
2024-02-08 13:54:15 +0900

    店舗詳細画面作成

--- GourmetSearch/Model/GetStoreDataModel.swift
+++ GourmetSearch/Model/GetStoreDataModel.swift
@@ -10,8 +10,8 @@ import Foundation
 class GetStoreDataModel {
     // 指定した検索範囲のお店情報を近い順に取得
     func getStoreData(range: Int, latitude: Double, longitude: Double) async throws -> StoreData {
-        let APIKey = ""
-        let url = URL(string: "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=\(APIKey)&lat=\(latitude)&lng=\(longitude)&range=\(range)&order=4&format=json")!
+        let APIKey = APIKey.APIKey
+        let url = URL(string: "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=\(APIKey)&lat=\(latitude)&lng=\(longitude)&range=\(range)&order=4&count=100&format=json")!
         let (data,_) = try await URLSession.shared.data(from: url)
         let decodedStoreData = try JSONDecoder().decode(StoreData.self, from: data)
         return decodedStoreData
--- GourmetSearch/ViewController/ResultViewController.swift
+++ GourmetSearch/ViewController/ResultViewController.swift
@@ -11,28 +11,72 @@ class ResultViewController: UIViewController{
     
     @IBOutlet weak var resultView: UITableView!
     
-    var shopsNumber: Int = 10    // 検索結果の件数
+    var shopsNumber: Int = 0    // 検索結果の件数
     var shops: [Shop] = []  // StoreDataのShop情報を入れる変数
-    
+    var ImageData: [UIImage] = []   // テーブルビュー内の写真をUIImage型で保存しておく変数
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        resultView.dataSource = self
         resultView.delegate = self
+        resultView.delegate = self
+        
+        
+        // バックグラウンドで取得したURLをUIImageに変換
+        DispatchQueue.global(qos: .userInteractive) .async {
+            var ImageDatas: [UIImage] = []
+            var numberInt: Int = 0
+            // TODO: URLに変換できなかった際のエラー処理を追記する必要あり
+            for _ in self.shops {
+                let urls = self.shops[numberInt].photo.mobile.l
+                numberInt += 1
+                if let url = URL(string: urls) {
+                    if let data = try? Data(contentsOf: url) {
+                        let UIImageData: UIImage = UIImage(data: data) ?? UIImage()
+                            ImageDatas.append(UIImageData)
+                    }
+                }
+            }
+            
+            self.ImageData = ImageDatas
+        }
+        
     }
+
 }
-extension ResultViewController: UITableViewDataSource, UITableViewDelegate {
+
+extension ResultViewController: UITableViewDataSource,UITableViewDelegate {
+    // 検索件数を設定
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print(shopsNumber)
-        print(shops)
         return shopsNumber
     }
+    
+    // セルのデザインを設定
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = resultView.dequeueReusableCell(withIdentifier: "ResultTableViewCell", for: indexPath)
-        
-        cell.textLabel?.text = "Shop \(indexPath.row + 1)"
+        let cell = resultView.dequeueReusableCell(withIdentifier: "ResultViewCell", for: indexPath)
+         cell.textLabel!.text = shops[indexPath.row].name
         
         return cell
     }
-        
+    
+    // タップしたセル番号を渡し、ShopInfoViewを開く処理へ
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedCellNumber = indexPath.row
+        // リザルトビューを開く処理へ
+        openShopInfoView(cellNumber: selectedCellNumber)
+    }
+    
+    // ShopInfoViewを開く処理
+    func openShopInfoView(cellNumber: Int) {
+        // ShopInfoViewControllerを取得
+        let storyboard = self.storyboard!
+        let shopInfoView = storyboard.instantiateViewController(withIdentifier: "ShopInfoViewController") as! ShopInfoViewController
+        shopInfoView.sentInfo = shops[cellNumber]
+        // TODO: ImageDataに情報を変換する処理を終えてから実行するようにする
+        if ImageData .isEmpty {
+            print("ImageDataが空白です")
+        } else {
+            shopInfoView.sentPhoto = ImageData[cellNumber]
+        }
+        navigationController?.pushViewController(shopInfoView, animated: true)
+    }
 }
