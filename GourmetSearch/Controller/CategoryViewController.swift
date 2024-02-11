5d393409e4e44c2f77ddcb608d2303011ee9ac55
hotta-ryohei <hotta.ryohei@gmail.com>
2024-02-11 22:20:17 +0900

    Tab Barを追加

--- GourmetSearch/Controller/CategoryViewController.swift
+++ GourmetSearch/Controller/CategoryViewController.swift
@@ -1,20 +1,18 @@
 //
-//  ZeroResultViewController.swift
+//  CategoryViewController.swift
 //  GourmetSearch
 //
 //  Created by 堀田凌平 on 2024/02/11.
 //
 
 import Foundation
-
 import UIKit
-class ZeroResultViewController: UIViewController{
-    
-    @IBOutlet var zeroResultViewController: UIView!
+
+class CategoryViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-    
+
 }
